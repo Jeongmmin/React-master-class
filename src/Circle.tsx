0d3f2627bc731ff917f6116c74cb8ef714b9e124
Jeongmmin <seo88kr@hanmail.net>
2022-04-06 00:35:34 +0900

    📝 study optional & state

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,20 +1,34 @@
+import { useState } from 'react';
 import styled from "styled-components";
 
 
-const Container = styled.div<CircleProps>`
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${props => props.bgColor};
   border-radius: 100px;
+  border: 2px solid ${props => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  // optional
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle( {bgColor} : CircleProps) {
+
+function Circle( {bgColor, borderColor, text =" default text "} : CircleProps) {
+
+
   return (
-    <Container bgColor={bgColor}/>
+    // borderColor가 undefined 상태라면 초기값 bgColor
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>{text}</Container>
   )
 }
 
