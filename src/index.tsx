48978ed055dad7043a92fccfb9982ac1fc947afc
Jeongmmin <seo88kr@hanmail.net>
2022-04-18 21:41:13 +0900

    🐛 Fix bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { RecoilRoot } from "recoil";
-import App from "./App";
-
-const queryClient = new QueryClient();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <ThemeProvider theme={lightTheme}>
+      </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+
